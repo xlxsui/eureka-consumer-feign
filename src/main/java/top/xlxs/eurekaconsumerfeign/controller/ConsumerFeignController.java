28e61a9b75ce59d432e82f6afbdd84eb16a1fbaf
jin <1542029827@qq.com>
2020-08-23 16:05:05 +0800

    eureka-consumer

---
+++ src/main/java/top/xlxs/eurekaconsumerfeign/controller/ConsumerFeignController.java
@@ -0,0 +1,28 @@
+package top.xlxs.eurekaconsumerfeign.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import top.xlxs.eurekaconsumerfeign.service.HiFeignService;
+
+@RestController
+@RequestMapping(value = "/consumer-feign", produces = "application/json; charset=utf-8")
+public class ConsumerFeignController {
+    private static final Logger logger = LoggerFactory.getLogger(ConsumerFeignController.class);
+
+    @Autowired
+    private HiFeignService hiFeignService;
+
+    @RequestMapping("/getHiFeign/{name}")
+    public String getHiFeign(@PathVariable String name) {
+        logger.info("************getHiFeign name:{}", name);
+        // 调用微服务
+        String result = hiFeignService.getHiFeign(name);
+        logger.info("************getHiFeign msg:{}", result);
+        return result;
+    }
+}
---
+++ src/test/java/top/xlxs/eurekaconsumerfeign/EurekaConsumerFeignApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xlxs.eurekaconsumerfeign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaConsumerFeignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
